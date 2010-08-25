7c3193c84dd868b5f5623d1a6c9f265201476d90
root <root@laxu.de>
2010-08-26 00:21:38 +0200

    str filter

---
+++ feedjack/templatetags/pretty_html.py
@@ -0,0 +1,14 @@
+from django import template
+from django.template.defaultfilters import *
+from BeautifulSoup import BeautifulSoup as soup
+
+register=template.Library()
+
+@register.filter
+@stringfilter
+def prettyhtml(value, autoescape=None):
+    value=str(soup(value).prettify())
+    if autoescape and not isinstance(value, SafeData):
+        from django.utils.html import escape
+        value = escape(value)
+    return mark_safe(value)
